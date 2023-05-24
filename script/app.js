f0634024ab57182b5a7abaa687a65b471cee7e9b
MDeBock <102190577+MDeBock@users.noreply.github.com>
2023-05-24 17:13:44 -0300

    ajustes

--- script/app.js
+++ script/app.js
@@ -96,14 +96,4 @@ function isEmail(email) {
 }
 
 
-function iniciarMap(){
-    var coord = {lat:-24.7965877 ,lng: -65.4390976};
-    var map = new google.maps.Map(document.getElementById('map'),{
-      zoom: 10,
-      center: coord
-    });
-    var marker = new google.maps.Marker({
-      position: coord,
-      map: map
-    });
-}
+
